4d0458a82ebde3c05147625c99b1eb8e50b2cee3
Raimond Spekking <raymond@users.mediawiki.org>
2012-03-08 10:33:55 +0000

    r113303: Consistency tweaks in preparation for adding extension to translatewiki.net * Fix description message key * First 'en', 2nd 'qqq', than the translations

--- SideBarMenu.i18n.php
+++ SideBarMenu.i18n.php
@@ -1,25 +1,32 @@
 <?php
-$messages = array();
-
-/** Message documentation (Message documentation)
+/**
+ * Internationalisation file for extension SideBarMenu
  *
+ * @file
+ * @ingroup Extensions
  */
-$messages['qqq'] = array(
-    'sidebarmenu-extension-description'             => '{{desc}}',
-    'sidebarmenu-parser-input-error'                => '$1 is the error message returned',
-    'sidebarmenu-parser-syntax-error'               => '$1 is the line which failed to be parsed',
-    'sidebarmenu-js-init-error'                     => 'General error message',
-    'sidebarmenu-parser-menuitem-expanded-null'    => 'parser.menuitem.expanded is a configuration property'
-);
+
+$messages = array();
 
 /** English
  *  @author Kim Eik
  */
 $messages['en'] = array(
-    'sidebarmenu-extension-description'             => 'A simple sidebar menu parser which creates collapsable/expandable menus and sub-menus.',
+    'sidebarmenu-desc'                              => 'A simple sidebar menu parser which creates collapsable/expandable menus and sub-menus',
     'sidebarmenu-parser-input-error'                => 'Parser returned with error: $1',
     'sidebarmenu-parser-syntax-error'               => 'Could not parse "$1", make sure the syntax is correct.',
     'sidebarmenu-js-init-error'                     => 'Failed loading JavaScript resources.',
-    'sidebarmenu-parser-menuitem-expanded-null'    => '\'parser.menuitem.expanded\' should be true or false, instead it\'s null.'
+    'sidebarmenu-parser-menuitem-expanded-null'     => '"parser.menuitem.expanded" should be true or false, instead it is null.'
+);
+
+/** Message documentation (Message documentation)
+ *  @author Kim Eik
+ */
+$messages['qqq'] = array(
+    'sidebarmenu-desc'                              => '{{desc}}',
+    'sidebarmenu-parser-input-error'                => '$1 is the error message returned',
+    'sidebarmenu-parser-syntax-error'               => '$1 is the line which failed to be parsed',
+    'sidebarmenu-js-init-error'                     => 'General error message',
+    'sidebarmenu-parser-menuitem-expanded-null'    => 'parser.menuitem.expanded is a configuration property'
 );
 
