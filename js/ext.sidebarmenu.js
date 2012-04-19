23a88b2e4d004295df82b39501f9b4d1995a9b7f
netbrain <netbrain@localhost>
2012-04-19 13:47:10 +0000

    minor js change

--- js/ext.sidebarmenu.js
+++ js/ext.sidebarmenu.js
@@ -2,13 +2,20 @@ $(document).ready(function () {
     if (typeof(sidebarmenu) !== 'undefined') {
         var showText = sidebarmenu.config.controls.show;
         var hideText = sidebarmenu.config.controls.hide;
-        var useAnimations = true;
+        var useAnimations = sidebarmenu.config.js.animate;
 
         function initControls() {
             $('.sidebar-menu-item-collapsed').children('.sidebar-menu-item-text-container').children('.sidebar-menu-item-controls').append(showText);
             $('.sidebar-menu-item-expanded').children('.sidebar-menu-item-text-container').children('.sidebar-menu-item-controls').append(hideText);
         }
 
+        /*Open submenu of current page if current page is present as a link in sidebarmenu*/
+        var selfLink = $('.sidebar-menu-item-collapsed').find('.selflink')[0]
+        if(selfLink !== undefined ){
+            $(selfLink).parents('.sidebar-menu-item-collapsed').removeClass('sidebar-menu-item-collapsed').addClass('sidebar-menu-item-expanded');
+        }
+
+        //initialize controls
         initControls();
         $('.sidebar-menu-item-controls').click(function () {
             var currentText = $(this).text();
@@ -31,12 +38,6 @@ $(document).ready(function () {
                 $(this).parents('.sidebar-menu-item:first').toggleClass('sidebar-menu-item-collapsed sidebar-menu-item-expanded');
             }
         });
-
-        /*Open submenu of current page if current page is present as a link in sidebarmenu*/
-        var selfLink = $('.sidebar-menu-item-collapsed').find('.selflink')[0]
-        if(selfLink !== undefined ){
-            $(selfLink).parents('.sidebar-menu-item-collapsed').removeClass('sidebar-menu-item-collapsed').addClass('sidebar-menu-item-expanded');
-        }
 
     } else {
         $('.sidebar-menu-container').prepend(mw.msg('sidebar-js-init-error'));
